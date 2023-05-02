497bbbbc99522c8cdd401bee9418d32f68085068
Polina Kasich <poli.key@yandex.ru>
2023-05-02 16:03:31 +0500

    commit 1

---
+++ py/lotus/entrance/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'entrance/index.html')
+
+def about(request):
+    return render(request, 'entrance/about.html')
+
+def breathe(request):
+    return render(request, 'entrance/breathe.html')
+
+def entr(request):
+    return render(request, 'entrance/entr.html')
+
+def nature(request):
+    return render(request, 'entrance/nature.html')
